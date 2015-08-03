1ca60752ab3c1be00c12f337ccb664ef51dafff5
Eduardo ES Riccardi <ed+git@kukino.uk>
2015-08-03 16:31:46 +0200

    from_str, display and others impl for Coord

--- src/base/coord.rs
+++ src/base/coord.rs
@@ -1,3 +1,5 @@
+use std::str::FromStr;
+use std::fmt::{Formatter, Error, Display};
 
 #[derive(Copy, Clone, PartialEq, Eq, Debug)]
 pub struct Coord {
@@ -18,10 +20,6 @@ impl Coord {
         Self::new(le_row as u8, le_col as u8)
     }
 
-    pub fn from_str(st : &str) -> Self {
-        Self::new(0,0)
-    }
-    
     pub fn all_possibles(board_size :usize) -> Vec<Self> {
         let mut coords = vec!();
         for x in 0..board_size {
@@ -31,4 +29,121 @@ impl Coord {
         }
         coords
     }
+}
+
+impl FromStr for Coord {
+    type Err = CoordParseError;
+
+    fn from_str(s: &str) -> Result<Coord, Self::Err> {
+
+        if s.len() < 2 {
+            return Err(CoordParseError(()))
+        }
+
+        let mut chars = s.chars();
+
+        // row
+        let row : u8;
+        if let Some(rowc) = chars.next() {
+            // the following might look awkard but it is X3 faster than string mangling.
+            let rowno = if rowc>='a' && rowc<='z' { rowc as u32 - 32 } else { rowc as u32 };
+            if rowno<'A' as u32 || rowno>'Z' as u32 || rowno == 'I' as u32 {
+                return Err(CoordParseError(()))
+            }
+            if rowno > 'I' as u32 {
+                row = (rowno - 'A' as u32 - 1) as u8;
+            } else {
+                row = (rowno - 'A' as u32) as u8;
+            }
+        } else {
+            return Err(CoordParseError(()))
+        }
+
+        // col
+        let col : u8;
+        match u8::from_str(&s[1..]) {
+            Ok(coln) => col = coln - 1,
+            Err(_) => return Err(CoordParseError(()))
+        }
+
+        Ok(Self::new(row,col))
+    }
+
+}
+
+impl Display for Coord {
+    fn fmt(&self, f: &mut Formatter) -> Result<(), Error> {
+        let rowc = if self.row>8 { (self.row+1+65) as char } else { (self.row+65) as char };
+        f.write_str(&format!("{}{}", rowc, self.col+1));
+        Ok(())
+    }
+
+}
+
+#[derive(Debug)]
+pub struct CoordParseError(());
+
+#[cfg(test)]
+mod test {
+
+    use std::str::FromStr;
+    use super::*;
+
+    #[test]
+    fn it_does_new() {
+        let c1 = Coord::new(0,1);
+        assert_eq!(0, c1.row);
+        assert_eq!(1, c1.col);
+    }
+
+    #[test]
+    fn it_eq_with_diff_constructors() {
+        let c1 = Coord::new(0,1);
+        let c2 = Coord::new_us(0,1);
+        assert_eq!(c1, c2);
+    }
+
+    #[test]
+    fn it_from_str_happy() {
+        match Coord::from_str(&"A2") {
+            Ok(c)  => assert_eq!(Coord::new(0,1), c),
+            Err(_) => panic!("Coord::form_str('A2') should be fine")
+        }
+    }
+
+    #[test]
+    fn it_does_with_coords_after_leter_i_and_two_digits() {
+        match Coord::from_str(&"T19") {
+            Ok(c)  => assert_eq!(Coord::new(18,18), c),
+            Err(_) => panic!("Coord::from_str('T19') should be raw row:col 18:18 (base 0)")
+        }
+    }
+
+    #[test]
+    fn it_from_str_anycase() {
+        assert_eq!(Coord::from_str(&"R18").unwrap(), Coord::from_str(&"r18").unwrap());
+    }
+
+    #[test]
+    fn it_from_str_err_on_empty() {
+        if let Ok(some) = Coord::from_str(&"") {
+            panic!(format!("empty string should Err() but we got {:?}", some))
+        }
+    }
+
+    #[test]
+    fn it_from_str_to_str_eq() {
+        for co in vec!("A1", "A2", "A3", "A20", "B3", "Z20", "G1", "H5") {
+            assert_eq!(format!("{}", Coord::from_str(&co).unwrap()), co);
+        }
+    }
+
+    // benchs
+    use test::Bencher;
+
+    #[bench]
+    fn bench_from_str(b: &mut Bencher) {
+        b.iter(|| Coord::from_str(&"R19").unwrap() );
+    }
+
 }
