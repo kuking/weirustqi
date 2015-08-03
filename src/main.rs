5c486196733d5bd80cdf2202cc287b8263c0e662
Eduardo ES Riccardi <ed+git@kukino.uk>
2015-08-03 12:38:14 +0200

    minor debug print

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,13 @@ use weirustqi::base::board::*;
 //use weirustqi::base::board;
 
 pub fn main() {
+    if cfg!(debug_assertions) {
+        println!("debug build")
+    } else {
+        println!("release build")
+    }
+
+
     println!("Hello wei-rust-qi");
     let b = Board::new(19);
     println!("{}", b);
