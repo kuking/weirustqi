ec00f887a63f9325b43707ee2f3d6905dcb28b05
Eduardo ES Riccardi <ed+git@kukino.uk>
2015-08-04 11:47:38 +0200

    integration test work in progress

--- src/base/tests/game.rs
+++ src/base/tests/game.rs
@@ -9,12 +9,19 @@ mod game_test {
     use base::moves::*;
     use base::color::*;
 
+    fn play_moves(game :&mut Game, moves : Vec<&str>) {
+        for m in moves {
+            assert!( game.play( Move::from_str(m).unwrap() ));
+        }
+    }
+
     //#[test]
     fn test_simple_kill() {
         let mut g = Game::new(19, 5.5, 0);
-        assert!( g.play( & Move::Stone(Coord::from_str(&"B1").unwrap(), Color::Black)) );
+        play_moves(&mut g, vec!(&"Black B1", &"White A1", &"Black A2") );
+
+        assert_eq!(Color::Empty, g.board().get(Coord::from_str("A1").unwrap()));
+        assert_eq!(1, g.dead_count(Color::White));
     }
-
-
 
 }
