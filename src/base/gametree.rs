1a18a91f639ba4dcd1571b8b701edccfa14417eb
Eduardo ES Riccardi <ed+git@kukino.uk>
2015-08-06 16:49:58 +0200

    basic functionality working for gametree #6

--- src/base/gametree.rs
+++ src/base/gametree.rs
@@ -15,21 +15,13 @@ pub struct GameTree {
 
 #[derive(Clone, Debug)]
 pub struct GameNode {
-    amove       :Move,
+    themove       :Move,
     comment     :String,
     variants    :LinkedList<GameNode>
 }
 
 
 impl GameTree {
-
-    pub fn parse(s : String) -> Self {
-        Self::new()
-    }
-
-    pub fn write() -> String {
-        String::new()
-    }
 
     pub fn new() -> GameTree {
         GameTree {
@@ -43,10 +35,10 @@ impl GameTree {
     }
 
     pub fn set_black_name(&mut self, black_name :String) { self.black_name = black_name }
-    pub fn black_name(&self) -> String { self.black_name.clone() }
+    pub fn black_name(&self) -> &String { &self.black_name }
 
     pub fn set_white_name(&mut self, white_name :String) { self.white_name = white_name }
-    pub fn white_name(&self) -> String { self.white_name.clone() }
+    pub fn white_name(&self) -> &String { &self.white_name }
 
     pub fn set_board_size(&mut self, board_size :usize) { self.board_size = board_size }
     pub fn board_size(&self) -> usize { self.board_size }
@@ -57,7 +49,33 @@ impl GameTree {
     pub fn set_handicap(&mut self, handicap :u16) { self.handicap = handicap }
     pub fn handicap(&self) -> u16 { self.handicap }
 
+    pub fn moves(&self) -> &LinkedList<GameNode> { &self.moves }
+    pub fn moves_as_mut<'r>(&'r mut self) -> &'r mut LinkedList<GameNode> { &mut self.moves }
 
+    pub fn push(&mut self, gn :GameNode) {
+        self.moves.push_back(gn);
+    }
+
+}
+
+impl GameNode {
+
+    pub fn new_simple(m :Move) -> GameNode {
+        GameNode { themove: m, comment: String::new(), variants: LinkedList::new() }
+    }
+
+    pub fn new(m :Move, s :&str) -> GameNode {
+        GameNode { themove: m, comment: s.to_string(), variants: LinkedList::new() }
+    }
+
+    pub fn push(& mut self, gn :GameNode) {
+        self.variants.push_back(gn)
+    }
+
+    pub fn themove(&self) -> Move { self.themove }
+    pub fn comment(&self) -> &String { &self.comment }
+    pub fn variants(&self) -> &LinkedList<GameNode> { &self.variants }
+    pub fn has_variants(&self) -> bool { !&self.variants.is_empty() }
 }
 
 // *********************************************************************************************
@@ -66,7 +84,14 @@ impl GameTree {
 #[cfg(test)]
 mod tests {
 
+    use std::str::FromStr;
+
     use super::*;
+    use base::moves::*;
+
+    fn amove(s :&str) -> Move {
+        Move::from_str(s).unwrap()
+    }
 
     #[test]
     fn it_can_be_created_and_retains_basic_properties() {
@@ -77,12 +102,84 @@ mod tests {
         gt.set_komi(5.5);
         gt.set_handicap(1);
 
-        assert_eq!("black".to_string(), gt.black_name());
-        assert_eq!("white".to_string(), gt.white_name());
+        assert_eq!(&"black".to_string(), gt.black_name());
+        assert_eq!(&"white".to_string(), gt.white_name());
         assert_eq!(19, gt.board_size());
         assert_eq!(5.5, gt.komi());
         assert_eq!(1, gt.handicap());
     }
+
+    #[test]
+    fn it_can_push_moves_into_it_simple() {
+        let mut gt = GameTree::new();
+        gt.push(GameNode::new_simple(amove("Black A1")));
+
+        assert_eq!(1, gt.moves.len());
+        let gn = gt.moves().front().unwrap();
+        assert_eq!(amove("Black A1"), gn.themove());
+        assert_eq!(&String::new(), gn.comment());
+        assert_eq!(0, gn.variants().len());
+    }
+
+    #[test]
+    fn it_can_push_moves_into_it_with_comments() {
+        let mut gt = GameTree::new();
+        gt.push(GameNode::new(amove("Black A1"), "a good move"));
+
+        assert_eq!(1, gt.moves.len());
+        let gn = gt.moves().front().unwrap();
+        assert_eq!(amove("Black A1"), gn.themove());
+        assert_eq!(&"a good move", gn.comment());
+        assert_eq!(0, gn.variants().len());
+    }
+
+    #[test]
+    fn it_can_push_moves_into_it_complex_with_variant() {
+        // B-A1 -> W-A2 -> B-A3 -> W-A4
+        //             \-> B-B3 -> W-B4
+
+        let mut gt = GameTree::new();
+        gt.push( GameNode::new_simple(amove("Black A1")) );
+        gt.push( GameNode::new_simple(amove("White A2")) );
+        gt.push( GameNode::new_simple(amove("Black A3")) );
+        gt.push( GameNode::new_simple(amove("White A4")) );
+        {
+            //let mut sub_node = gt.moves_as_mut().front_mut().unwrap();
+            let mut sub_node = gt.moves_as_mut().iter_mut().nth(1).unwrap();
+            sub_node.push( GameNode::new_simple(amove("Black B3")) );
+            sub_node.push( GameNode::new_simple(amove("White B4")) );
+        }
+
+        // normal path
+        {
+            let mut iter = gt.moves().iter();
+            assert_eq!(amove("Black A1"), iter.next().unwrap().themove() );
+            assert_eq!(amove("White A2"), iter.next().unwrap().themove() );
+            assert_eq!(amove("Black A3"), iter.next().unwrap().themove() );
+            assert_eq!(amove("White A4"), iter.next().unwrap().themove() );
+            assert!( iter.next().is_none() );
+        }
+
+        // variants checking, only on the second should have a variant
+        {
+            let mut iter = gt.moves().iter();
+            assert!(!iter.next().unwrap().has_variants() );
+            assert!( iter.next().unwrap().has_variants() );
+            assert!(!iter.next().unwrap().has_variants() );
+            assert!(!iter.next().unwrap().has_variants() );
+        }
+
+        // now lets check the variants
+        {
+            let mut iter = gt.moves().iter();
+            let gn = iter.nth(1).unwrap();
+            let mut gn_iter = gn.variants().iter();
+            assert_eq!(amove("Black B3"),  gn_iter.next().unwrap().themove() );
+            assert_eq!(amove("White B4"),  gn_iter.next().unwrap().themove() );
+            assert!( gn_iter.next().is_none() );
+        }
+    }
+
 
 
 }
