6cdbf9aea96bb1a63385ad1e9b4fc85afc34bebf
Eduardo ES Riccardi <ed+git@kukino.uk>
2015-08-13 18:37:18 +0200

    it can use a GameResultRange now #7

--- src/mcts/mred.rs
+++ src/mcts/mred.rs
@@ -7,7 +7,7 @@ pub struct MrEd<'r> {
     cache     :game_state::GameStateCache,
     ministers :Vec<&'r minister::Minister>,
     timer     :&'r time_keeper::TimeKeeper,
-    scorer    :fn(&game::Game) -> game_result::GameResult
+    scorer    :fn(&game::Game) -> game_result::GameResultRange
 }
 
 impl<'r> MrEd<'r> {
@@ -15,7 +15,7 @@ impl<'r> MrEd<'r> {
     pub fn new(game      :game::Game,
                ministers :Vec<&'r minister::Minister>,
                timer     :&'r time_keeper::TimeKeeper,
-               scorer    :fn(&game::Game) -> game_result::GameResult) -> MrEd<'r> {
+               scorer    :fn(&game::Game) -> game_result::GameResultRange) -> MrEd<'r> {
 
 
         let game_state_cache = game_state::GameStateCache::new((&game).board().size());
