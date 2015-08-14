66ecce0e6a489fdf52c9a32e74b48d6602ca1f43
Eduardo ES Riccardi <ed+git@kukino.uk>
2015-08-14 15:01:29 +0200

    some docs in regards to votes #10

--- src/mcts/analytics/vote.rs
+++ src/mcts/analytics/vote.rs
@@ -1,6 +1,9 @@
 
 use base::*;
 
+/// Votes should be in a way that are usefull for both White and Black. i.e. if it is a very good
+/// move for Black, but this board configuration is now analysed for White-to-play, the vote
+/// should highlight the fact that a position is to be taken care by white too, i.e. by connecting.
 pub enum Vote {
     Avoid(coord::Coord),
     Vote(coord::Coord, i8)
